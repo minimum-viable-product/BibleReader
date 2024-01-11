4b77d12b76fb843ba2fa0906bea81515987065d8
veganaiZe <veganaiZe@protonmail.com>
2024-01-11 07:01:41 -0800

    Refactor tests

--- BibleReaderTest.java
+++ BibleReaderTest.java
@@ -1,27 +1,35 @@
 class BibleReaderTest {
-    static {  // Ensures asserts are enabled
-        boolean assertsEnabled = false; assert assertsEnabled = true;
-        if (! assertsEnabled) throw new RuntimeException("Asserts NOT enabled!");
+    static {
+        boolean assertsEnabled = false;
+        assert assertsEnabled = true;
+        if (! assertsEnabled) throw new RuntimeException(
+                "ASSERTIONS MUST BE ENABLED WITH: -ea"
+        );
     }
 
     public static void main(String[] args) {
-        assert new Bible().openBookChapter("genesis", 1).equals(
-                "In the beginning God created the heaven and the earth.")
-                : "Text doesn't match.";
+        assert new Bible()
+                .openBookChapter("genesis", 1)
+                .equals("In the beginning God created the heaven and the earth.")
+                : "TEXT SHOULD MATCH";
 
-        assert new Bible().openBookChapter("genesis").equals(
-                "In the beginning God created the heaven and the earth.")
-                : "Text doesn't match.";
+        assert new Bible()
+                .openBookChapter("genesis")
+                .equals("In the beginning God created the heaven and the earth.")
+                : "TEXT SHOULD MATCH";
 
-        assert new Bible().openBookChapter().equals(
-                "In the beginning God created the heaven and the earth.")
-                : "Text doesn't match.";
+        assert new Bible()
+                .openBookChapter()
+                .equals("In the beginning God created the heaven and the earth.")
+                : "TEXT SHOULD MATCH";
 
-        assert new Bible().openBookChapter("genesis", 2).equals(
-                "And the earth was without form, and void; and darkness was upon the "+
-                "face of the deep. And the Spirit of God moved upon the face of the "+
-                "waters.")
-                : "Text doesn't match.";
+        assert new Bible()
+                .openBookChapter("genesis", 2)
+                .equals("And the earth was without form, and void; and darkness was upon the "+
+                        "face of the deep. And the Spirit of God moved upon the face of the "+
+                        "waters.")
+                : "TEXT SHOULD MATCH";
+
 
         System.out.println("All tests passed.");
     }
