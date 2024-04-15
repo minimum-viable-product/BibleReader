67b7a29b562c9cbdd3266b0971991ff210dcb979
veganaiZe <veganaiZe@protonmail.com>
2024-04-15 16:32:53 -0700

    Discover and run all class tests in default, unnamed package

--- src/TestRunner.java
+++ src/TestRunner.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 
@@ -15,12 +17,18 @@ class TestRunner {
         if (! assertion) throw assertionError;
     }
 
+
     public static void
     main(String[] args) {
-        runLowLevelTests();
-        runHighLevelTests();
-        System.out.println("All tests pass!");
+        String packageName = args[0];
+
+        runAllTestClasses(packageName);
+        //runLowLevelTests();
+        //runHighLevelTests();
+
+        System.out.println("\nTesting complete.");
     }
+
 
     static int
     runLowLevelTests() {
@@ -29,10 +37,49 @@ class TestRunner {
         return 0;
     }
 
+
     static int
     runHighLevelTests() {
         BibleTests.main(new String[0]);
         System.out.println("High level testing completed.");
+        return 0;
+    }
+
+
+    static int
+    runAllTestClasses(String packageName) {
+        if (packageName.equals(".")) {
+            packageName = "./";
+        } else {
+            packageName = packageName.replace('.', '/');
+        }
+
+        File[] files = new File(packageName).listFiles();
+        if (files == null) {
+            System.err.println("ERROR: Bad package name");
+            System.exit(1);
+        }
+
+        for (int i=0; i < files.length; ++i) {
+            String fileName = files[i].getName();
+            if (fileName.endsWith("Tests.class")) {
+                //URL resource = Temp.class.getResource(fileName);
+                String name = fileName.substring(0, fileName.length()-6);
+                try {
+                    //System.out.println(Class.forName(name).getName());
+                    Class cls = Class.forName(name);
+                    Method method = cls.getMethod(
+                            "main",
+                            new Class[] { String[].class }
+                    );
+                    method.invoke(new Object(), new Object[] { new String[0] });
+                } catch (java.lang.ReflectiveOperationException e) {
+                    System.err.println(e);
+                    System.exit(1);
+                }
+            }
+        }
+
         return 0;
     }
 
