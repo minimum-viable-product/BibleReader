8391e97048e099460cdb02236a554a4c0213b58a
veganaiZe <veganaiZe@protonmail.com>
2024-07-15 21:18:59 -0700

    Take command line args

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,15 @@
 class Main {
     public static void
     main(String[] args) {
-        System.out.println(Bible.open());
+        String book = args[0];
+        String chapter = args[1];
+        String verse = args[2];
+        System.out.println(
+                Bible.open(
+                        book,
+                        Integer.parseInt(chapter),
+                        Integer.parseInt(verse)
+                )
+        );
     }
 }
